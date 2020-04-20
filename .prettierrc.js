1b494d2e2b5ab94cde7003e6fbe033e6038f5618
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-04-20 23:58:59 +0530

    base setup made

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  bracketSpacing: false,
+  printWidth: 120
+};
