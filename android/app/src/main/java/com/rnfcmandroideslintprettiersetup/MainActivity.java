739e36d84fb415516b5c299796f86c2a426cf718
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-04-21 23:01:18 +0530

    localnotification setup done

--- android/app/src/main/java/com/rnfcmandroideslintprettiersetup/MainActivity.java
+++ android/app/src/main/java/com/rnfcmandroideslintprettiersetup/MainActivity.java
@@ -1,26 +1,23 @@
 package com.rnfcmandroideslintprettiersetup;
 
+
+
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RNFCMAndroidEslintPrettierSetup";
   }
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-   return new ReactActivityDelegate(this, getMainComponentName()) {
-     @Override
-     protected ReactRootView createRootView() {
-      return new RNGestureHandlerEnabledRootView(MainActivity.this);
-     }
-   };
- }
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {    
       NotificationChannel notificationChannel = new NotificationChannel("500", "MainChannel", NotificationManager.IMPORTANCE_HIGH);
       notificationChannel.setShowBadge(true);
